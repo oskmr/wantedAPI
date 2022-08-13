3a971043a0bd2932d67ee4e2354f506029a049c1
miseri osaka <str3tech@gmail.com>
2022-08-13 14:41:01 +0900

    Initial Commit

---
+++ wantedAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wantedAPI
+//
+//  Created by 逢坂 美芹 on 2022/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
